1f07cecd4457bbbfc19aac5711c6c08136d52949
HuysGithub <august16.hn@gmail.com>
2024-02-18 22:05:21 +0700

    refactor: practice 1 version 2 - focus on main functions\nRewrite the practice 1 to align with the main requirements

---
+++ 1.student.mark.ver2.py
@@ -0,0 +1,61 @@
+# input student function
+def inputStudent():
+    Id = input("ID: ")
+    Name = input("Name: ")
+    DoB = input("DoB: ")
+    # return a dict
+    return {"ID": Id, "Name": Name, "DoB" : DoB, "Mark" : {}}
+
+# input course function
+def inputCourse():
+    CourseID = input("Course ID: ")
+    CourseName = input("Course Name: ")
+    return {"Course ID": CourseID, "Course Name": CourseName}
+
+# listing functions
+def listingStudent(students):
+    print("Student information:")
+    for student in students:
+        print(f"{student.get('ID')}\t{student.get('Name')}\t{student.get('DoB')}")
+def listingCourses(courses):
+    print("Course information:")
+    for course in courses:
+        print(f"{course.get('Course ID')}\t{course.get('Course Name')}")
+    
+def main():
+    students = []
+    courses = []
+    
+    numberOfStudent = 0
+    numberOfCourse = 0
+    
+    # input student
+    numberOfStudent = input("How many students? \n> ")
+    for _ in numberOfStudent:
+        student = inputStudent() # one student as a dict
+        students.append(student)
+    
+    # input course
+    numberOfCourse = input("How many courses?\n> ")
+    for _ in numberOfCourse:
+        course = inputCourse()
+        courses.append(course)
+        
+    # select course to input mark
+    courseID = input("Input Course ID or Course Name to input mark:\n> ").lower()
+    addStatus = False
+    for course in courses:
+        if course["Course ID"].lower() == courseID or course["Course Name"].lower() == courseID:
+            addStatus = True
+            for student in students:
+                mark = float(input(f"Input mark for {student.get('ID')} - {student.get('Name')}: "))
+                student['Mark'] = {course.get('Course ID') : mark}
+    if addStatus == False:
+        print(f"Course {courseID} Not Found")        
+
+    listingStudent(students)
+    
+    listingCourses(courses)
+
+if __name__ == "__main__":
+    main()
